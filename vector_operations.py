e3861c36d02f60677de8ac6c05158a4dbfb25d5d
Veeresh SM <veeresh.sm@zebra.com>
2022-02-09 18:27:37 +0530

    Making first commit

---
+++ vector_operations.py
@@ -0,0 +1,28 @@
+import math
+import numpy as np
+
+def dotproduct(v1, v2):
+  return sum((a*b) for a, b in zip(v1, v2))
+
+def length(v):
+  return math.sqrt(dotproduct(v, v))
+
+def angle(v1, v2):
+  return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+            # >>> angle_between((1, 0, 0), (0, 1, 0))
+            # 1.5707963267948966
+            # >>> angle_between((1, 0, 0), (1, 0, 0))
+            # 0.0
+            # >>> angle_between((1, 0, 0), (-1, 0, 0))
+            # 3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
